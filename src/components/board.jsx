1d57f8cf41040483995ce7b5998e1fa6640a0353
peketirajesh007@gmail.com <you@example.com>
2023-06-27 12:06:22 +0530

    added click event

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,22 +1,42 @@
 import Square from "./square";
-
+import { useState } from "react";
 const Board = () => {
+
+    const [squares, setSquares] = useState(Array(9).fill(null))
+
+    const handleSquareClick = (clickedPosition) => {
+        setSquares(currentSquares => {
+            return currentSquares.map((squareValue, position) => {
+                if (clickedPosition === position) {
+                    console.log(squareValue)
+                    console.log(position)
+                    return "X"
+                }
+                return squareValue
+            })
+        })
+    }
+
+    const renderPosition = (position) => {
+        return <Square value={squares[position]} onClick={() => handleSquareClick(position)} />
+    }
+
     return (
         <div className="board">
             <div className="board-row">
-                <Square value={0} />
-                <Square value={1} />
-                <Square value={2} />
+                {renderPosition(0)}
+                {renderPosition(1)}
+                {renderPosition(2)}
             </div>
             <div className="board-row">
-                <Square value={3} />
-                <Square value={4} />
-                <Square value={5} />
+                {renderPosition(3)}
+                {renderPosition(4)}
+                {renderPosition(5)}
             </div>
             <div className="board-row">
-                <Square value={6} />
-                <Square value={7} />
-                <Square value={8} />
+                {renderPosition(6)}
+                {renderPosition(7)}
+                {renderPosition(8)}
             </div>
         </div>
     );
