d52855e1cab9c98163cf1d4313d6826be65a4c9f
peketirajesh007@gmail.com <you@example.com>
2023-06-27 08:19:18 +0530

    installed sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css'
+import Board from './components/board';
+import './components/styles.scss'
+
 
 function App() {
 
   return (
-    <>
-
-    </>
-  )
-}
+    <div className='app'>
+      <Board />
+    </div>
+  );
+};
 
 export default App
